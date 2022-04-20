27d42c7dcbcc2268b63aedc442e9a577bc14d0ac
Steven <s3@uwaterloo.ca>
2022-04-20 20:42:45 +0700

    add gtag and contact.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import '../styles/globals.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -8,6 +9,18 @@ function MyApp({ Component, pageProps }) {
         <title>Cipta Khasanah Blasting</title>
         <link rel="shortcut icon" href="/ckbLogo.ico" />
       </Head>
+      <Script
+         strategy="lazyOnload"
+         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+       />
+       <Script id="gtag" strategy="lazyOnload">
+         {`
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+           gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+         `}
+       </Script>
       <Component {...pageProps} />
     </div>
   )
---
+++ pages/api/contact.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+const PWD = process.env.password
+
+export default function contact(req, res) {
+  // smtp information
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+      user: 'dwiprimawebsite@gmail.com',
+      pass: PWD,
+    },
+    secure: true,
+  });
+
+  const mailData = {
+    from: 'dwiprimakaryaguna@gmail.com',
+    to: 'steven.infinity29@gmail.com',
+    subject: `${req.body.subject}`,
+    text: req.body.message,
+    html: `<div>
+      Name: ${req.body.name}\n
+      Company: ${req.body.company}\n
+      Phone: ${req.body.phoneNumber}\n
+      Email: ${req.body.email}\n
+      Subject: ${req.body.subject}\n
+      ${req.body.message}
+    </div>`
+  }
+
+  transporter.sendMail(mailData, function (err, info) {
+    if (err){
+      console.error(err);
+    }
+    else{
+      console.log(info)
+    }
+  })
+  console.log('replying');
+
+  res.status(200).send('Message Sent!');
+}
