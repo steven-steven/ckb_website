255e2e21a9dd4dd1a0b814b72ea1e4a693024a06
Steven <steven.steven@shopify.com>
2024-11-12 19:12:05 -0500

    Add metadata

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,21 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Cipta Khasanah Blasting</title>
         <link rel="shortcut icon" href="/ckbLogo.ico" />
+        <meta name="description" content="Cipta Khasanah Blasting - Indonesia's top sandblasting company" />
+        <meta name="keywords" content="sandblasting, coating, company, contractor" />
+        {/* Open Graph Meta Tags */}
+        <meta property="og:url" content="https://ckb.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Cipta Khasanah Blasting" />
+        <meta property="og:description" content="Cipta Khasanah Blasting - Indonesia's top sandblasting company" />
+        <meta property="og:image" content="https://ckb.vercel.app/img/redpipe.png" />
+        <meta property="og:image:alt" content="Cipta Khasanah Blasting" />
+        {/* Twitter Card Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Cipta Khasanah Blasting" />
+        <meta name="twitter:description" content="Cipta Khasanah Blasting - Indonesia's top sandblasting company" />
+        <meta name="twitter:image" content="https://ckb.vercel.app/img/redpipe.png" />
+        <meta name="twitter:image:alt" content="Cipta Khasanah Blasting" />
       </Head>
       <Script
          strategy="lazyOnload"
